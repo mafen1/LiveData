60c84878a016f9dffeb5feac9d959ecdc1222fe5
ok <66274954+mafen1@users.noreply.github.com>
2022-03-19 23:31:13 +0300

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -65,7 +65,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
     }
-    fun String.toEditable(): Editable =  Editable.Factory.getInstance().newEditable(this)
 }
 
 
